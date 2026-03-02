3262070defd7edc42ce5d58b6fe90b00d2f2591b
Po Po <pop504489@gmail.com>
2026-03-02 18:17:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5177938032-5ead4",
+  "appId": "1:1051283675785:web:67d4ab5678e35ddef6c458",
+  "apiKey": "AIzaSyCnz4b0LZiI539EwX1Ra-bNK4nHN6nZFzk",
+  "authDomain": "studio-5177938032-5ead4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1051283675785"
+};
